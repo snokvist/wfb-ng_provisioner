1c78254341130008549a77fdbce430d555a000fe
snokvist <joakim.snokvist@gmail.com>
2025-02-14 17:32:18 +0100

    Add files via upload

--- drone/usr/bin/temperature_check.sh
+++ drone/usr/bin/temperature_check.sh
@@ -3,7 +3,7 @@
 WARNING1_THRESHOLD=80       # First warning threshold
 WARNING2_THRESHOLD=90       # Second warning: add "VTX will soon thermal throttle..."
 THROTTLE_THRESHOLD=100      # Throttle action threshold: adjust TX power and set FPS
-REBOOT_THRESHOLD=112        # Reboot threshold
+REBOOT_THRESHOLD=110       # Reboot threshold
 
 while true; do
     # --- Get VTX Temperature ---
@@ -28,6 +28,7 @@ while true; do
         adapter_temp=0
     elif [ "$wifi_adapter" = "8812eu" ]; then
         # Placeholder for extraction for 8812eu:
+        #cat /proc/net/rtl88x2eu/<wlan0>/thermal_state 
         echo "Adapter 8812eu temperature extraction not implemented yet."
         adapter_temp=0
     else
@@ -51,6 +52,8 @@ while true; do
     # --- Check Thresholds and Take Actions ---
     if [ "$max_temp" -ge "$REBOOT_THRESHOLD" ]; then
         echo "VTX will reboot due to thermal state..."
+	echo "Rebooting in 5seconds...VTX Temp:&T WifiTemp: &W &L33 &F24 CPU: &C Bitrate: &B" > /tmp/msposd.msg
+	sleep 5
         reboot
     elif [ "$max_temp" -ge "$THROTTLE_THRESHOLD" ]; then
         # For adapter type 88XXau, use a negative value in the txpower command.
@@ -61,11 +64,11 @@ while true; do
         fi
         iw dev wlan0 set txpower fixed $txpower_value
         echo setfps 0 30 > /proc/mi_modules/mi_sensor/mi_sensor0
-        echo "Throttling VTX. Reboot imminent, return to home..."
+        echo "Throttling VTX. Reboot imminent, return to home..VTX Temp:&T WifiTemp: &W &L23 &F34 CPU: &C Bitrate: &B" > /tmp/msposd.msg
     elif [ "$max_temp" -ge "$WARNING2_THRESHOLD" ]; then
-        echo "Warning: High temperature detected. VTX will soon thermal throttle..."
+        echo "Warning: High temperature detected. VTX will soon thermal throttle...VTX Temp:&T WifiTemp: &W &L33 &F24 CPU: &C Bitrate: &B" > /tmp/msposd.msg
     elif [ "$max_temp" -ge "$WARNING1_THRESHOLD" ]; then
-        echo "Warning: High temperature detected."
+        echo "Warning: High temperature detected. VTX Temp:&T WifiTemp: &W &L33 &F44 CPU: &C Bitrate: &B" > /tmp/msposd.msg
     fi
 
     # Check again every 5 seconds
