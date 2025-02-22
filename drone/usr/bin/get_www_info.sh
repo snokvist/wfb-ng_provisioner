09fc0c8ce307d282dfb2ea64ea1b9e365987e5e1
snokvist <joakim.snokvist@gmail.com>
2025-02-22 11:33:12 +0100

    Update get_www_info.sh

--- drone/usr/bin/get_www_info.sh
+++ drone/usr/bin/get_www_info.sh
@@ -2,5 +2,5 @@
 
 ##Add code to print out temp etc for the web interface.
 #soc temp, wifi temp, current wfb settings, current wifi tx_pwr
-ipcinfo -t > /tmp/vtx.log
-ipcinfo -t >> /tmp/www_info
+ipcinfo -t >> /tmp/vtx.log
+ipcinfo -t > /tmp/www_info
